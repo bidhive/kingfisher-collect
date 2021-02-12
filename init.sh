98a5ceeac46b75081715fe37bf94e1801bc1faca
alecbass <alec.bassingthwaighte@gmail.com>
2021-02-12 11:48:15 +1000

    Change $count to $COUNT in the contracts server startup script.

--- init.sh
+++ init.sh
@@ -6,6 +6,6 @@ then
     COUNT=10
 fi
 
-scrapy crawl australia -a from_date=2021-02-01 -a sample=$count
+scrapy crawl australia -a from_date=2021-02-01 -a sample=$COUNT
 export FLASK_APP=server.py
 python -m flask run
